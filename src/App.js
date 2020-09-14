412e19a886622fe3ad3404ecfb1b2997fcd5c254
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-09-14 11:16:31 +0200

    initial commint

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Second rect firebase attemp</h1>
+    </div>
+  );
+}
+
+export default App;
